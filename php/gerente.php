03bdd33135710d1864868a7567484b3dcd316911
Lívia Nascimento <livialuz145@gmail.com>
2025-03-11 18:06:50 -0300

    cadastro gerente

---
+++ php/gerente.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("conexao.php");
+
+    function CadastrarGerente(){
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+    }
+?>
