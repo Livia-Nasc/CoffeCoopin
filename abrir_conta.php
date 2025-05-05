561d3e26758b2b599ec02639a71b18ad923e9ad4
Livia-Nasc <livialuz145@gmail.com>
2025-05-05 19:10:43 -0300

    conta

--- abrir_conta.php
+++ abrir_conta.php
@@ -33,7 +33,7 @@ $sql_garcom = "SELECT id FROM garcom WHERE user_id = :user_id ";
 $stmt = $conn->prepare($sql_garcom);
 $stmt->bindParam(':user_id', $user_id);
 $stmt->execute();
-$dadosUsuario = $stmt->fetch(PDO::FETCH_ASSOC);
+$dadosUsuario = $stmt->fetch();
 $garcom_id = $dadosUsuario['id'];
 ?>
 
